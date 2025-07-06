df4b1e760ba9da6794c02f974943d5a9cecf6130
Atul Kumar Gupta <atulgupta3058@gmail.com>
2025-07-06 22:54:15 +0530

    React Native Expo Setup with Tailwind

---
+++ frontend/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default Index;
